dfe82e66f07569376c0741c3077bfae3edcf2e10
Guilherme Cardoso <gchicoper@gmail.com>
2021-01-28 00:46:56 -0300

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// in src/App.js
+import * as React from "react";
+import { Admin, Resource} from 'react-admin';
+import { UserList } from './users';
+import { PostList, PostEdit, PostCreate } from './posts';
+import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import Dashboard from './Dashboard';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => (
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} 
+      edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+    <Resource name="users" list={UserList} icon={UserIcon}/>
+  </Admin>
+);
+
+export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import {Card, CardContent, CardHeader} from '@material-ui/core';
+
+export default () => (
+    <Card>
+        <CardHeader title="Welcome" />
+        <CardContent>
+            Lorem ipsum presuntum e queijum
+        </CardContent>
+    </Card>
+);
